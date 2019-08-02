41a83b133d4d9a359c5cea0960169ff626a5bf74
lmcandoit1986 <lmcandoit@126.com>
2019-08-02 10:29:06 +0800

    文件布局修改

--- Web/htmlRoute.py
+++ Web/htmlRoute.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 from django.template.loader import get_template
 
 from Server import Server
-from Server.models import resultAll
 
 def index(request):
     t = get_template('index.html')
@@ -17,13 +16,12 @@ def APIWatcher(request):
     res = Server.getAPIMonitorDataJson(request)
     res_dict = eval(res)
     if res_dict['code'] == -1:
-        # #print("fail")
+        # print("fail")
         context = {'person': None}
     else:
-        # #print('Pass')
+        # print('Pass')
         context = {'person': res,'dict_data':res_dict}
     # print(context)
-
     return render(request,'CheckAPI.html',context)
 
 def getAndroidList(request):
@@ -31,10 +29,10 @@ def getAndroidList(request):
     res_dict = eval(res)
     res2 =Server.getRate(request)
     if res_dict['code'] == -1:
-        # #print("fail")
+        # print("fail")
         context = {'person': None}
     else:
-        # #print('Pass')
+        # print('Pass')
         context = {'person': res_dict,'rate':res2}
     # print(context)
     return render(request, 'listAndroid.html', context)
@@ -43,12 +41,12 @@ def getAPIMonitorList(request):
     res = Server.getAPIMonitorRateJson(request)
     res_dict = eval(res)
     if res_dict['code'] == -1:
-        # #print("fail")
+        # print("fail")
         context = {'person': None}
     else:
-        # #print('Pass')
+        # print('Pass')
         context = {'person': res,'JS':res_dict}
-    # #print(context)
+    # print(context)
     return render(request, 'listAPIMonitor.html', context)
 
 def getiOSList(request):
@@ -56,79 +54,71 @@ def getiOSList(request):
     res_dict = eval(res)
     res2 = Server.getRate(request)
     if res_dict['code'] == -1:
-        # #print("fail")
+        # print("fail")
         context = {'person': None}
     else:
-        # #print('Pass')
+        # print('Pass')
         context = {'person': res_dict, 'rate': res2}
-    # #print(context)
+    # print(context)
     return render(request, 'listiOS.html', context)
-    # t = get_template('listiOS.html')
-    # html = t.render()
-    # return HttpResponse(html)
 
 def Deatail(request):
     res = Server.getResults(request)
     res_dict = eval(res)
     if res_dict['code'] == -1:
-        ##print("fail")
+        # print("fail")
         context = {'person': None}
     else:
-        ##print('Pass')
+        # print('Pass')
         context = {'person': res_dict}
-    # #print(context)
+    # print(context)
     return render(request, 'resultDetail.html', context)
 
 def statistics(request):
     res = Server.getRate(request)
     res_dict = eval(res)
     if res_dict['code'] == -1:
-        # #print("fail")
+        # print("fail")
         context = {'personjson': None}
     else:
-        # #print('Pass')
+        # print('Pass')
         context = {'personjson':res}
-    # #print(context)
+    # print(context)
     return render(request, 'statistics.html', context)
-    # t = get_template('statistics.html')
-    # html = t.render()
-    # return HttpResponse(html)
 
 def performanceListAndroid(request):
     res = Server.getPTResultslistJson(request)
     res_dict =eval(res)
     if res_dict['code'] == -1:
-        # #print("fail")
+        # print("fail")
         context = {'person': None}
     else:
-        # #print('Pass')
+        # print('Pass')
         context = {'person':res_dict}
-    # #print(context)
+    # print(context)
     return render(request, 'performanceList.html', context)
-    # t = get_template('performanceList.html')
-    # html = t.render()
-    # return HttpResponse(html)
+
 
 def performanceListiOS(request):
     res = Server.getPTResultslistJson(request)
     res_dict = eval(res)
     if res_dict['code'] == -1:
-        # #print("fail")
+        # print("fail")
         context = {'person': None}
     else:
-        # #print('Pass')
+        # print('Pass')
         context = {'person': res_dict}
-    # #print(context)
+    # print(context)
     return render(request, 'performanceListiOS.html', context)
 
 def performance(request):
     res = Server.getPtResultsJson(request)
     res_dict = eval(res)
     if res_dict['code'] == -1:
-        ##print("fail")
+        # print("fail")
         context = {'person': None}
     else:
-        # #print('Pass')
+        # print('Pass')
         context = {'person': res_dict,'jss':res}
-    ##print(context)
+    # print(context)
     return render(request, 'performance.html', context)
