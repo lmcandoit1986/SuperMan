824c518775850e3425de12b01f328c748918cb34
lmcandoit1986 <lmcandoit@126.com>
2019-08-02 14:23:51 +0800

    s

--- SuperMan/wsgi.py
+++ SuperMan/wsgi.py
@@ -10,11 +10,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 import sys
 
-from os.path import dirname, abspath
-
-PROJECT_DIR = dirname(dirname(abspath(__file__)))
-sys.path.insert(0, PROJECT_DIR)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SuperMan.settings")
+sys.path.append('/usr/local/lib/python3.5/dist-packages')
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SuperMan.settings")
 application = get_wsgi_application()
