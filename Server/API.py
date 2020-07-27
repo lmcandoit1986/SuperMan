4c83a5de65b54b86bb94e495258a968d535450e7
liminghn <liming@ycfin.com.cn>
2020-07-27 09:19:03 +0800

    日志记录

--- Server/API.py
+++ Server/API.py
@@ -226,6 +226,7 @@ def api_server_list(request):
         type = request.GET['type']
     else:
         return simplejson.dumps({'code': -1, 'msg': '暂不支持该请求方式'})
+
     objectapi = APIrunlist.objects.filter(type=type).order_by('-id')[0:30]
     Result = {}
     autolist = []
